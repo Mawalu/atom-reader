dfa5fb5e4a9016e2e460b70b0abb06dfa1d8d792
mawalu <web@mawalabs.de>
2015-03-20 23:41:00 +0000

    moved project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# feedreader
+This project has moved and is now available [here](https://github.com/niklasbuschmann/atom-angular)
--- api/policies/setOwner.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(req, res, next) {
-  if (!req.session.authenticated) return res.send("Forbidden", 403);
-  if (req.body) req.body.owner = req.session.user.id;
-  req.query.owner = req.session.user.id;
-  next();
-}
